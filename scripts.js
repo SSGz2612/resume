d45cd3fd4d40a3b67baf40a6a37e97dbb27c96ba
Sergio <sduarte2612@gmail.com>
2022-01-28 10:39:09 +0200

    add javascript functions -NAV-

--- scripts.js
+++ scripts.js
@@ -1,59 +1,105 @@
+var x1 = document.getElementById( "nav_c1_1" );
+var x2 = document.getElementById( "nav_c1_1_1" );
+var w1 = document.getElementById( "nav_c1_2" );
+var y1 = document.getElementById( "nav_c1_3" );
+var z1 = document.getElementById( "nav_c1_4" );
+
+var v1 = document.getElementById( "nav_d1_1" );
+var v2 = document.getElementById( "nav_d1_2" );
+
+var t1 = document.getElementById( "nav_d1_2_1" );
+
+var u1 = document.getElementById( "nav_d1_2_1_1" );
+var u2 = document.getElementById( "nav_d1_2_1_2" );
+var u3 = document.getElementById( "nav_d1_2_1_3" );
+var u4 = document.getElementById( "nav_d1_2_1_4" );
+var u5 = document.getElementById( "nav_d1_2_1_5" );
+var u6 = document.getElementById( "nav_d1_2_1_6" );
+var u7 = document.getElementById( "nav_d1_2_1_7" );
+
+var engL = [ "About me", "Knowledges", "Projects" ];
+var ukrL = [ "Про мене", "Знання", "Проекти" ];
+var rusL = [ "Обо мне", "Знания", "Проекты" ];
+
+function changeL( lan ) {
+    for( let x = 0; x < lan.length; x++ ) {
+        document.getElementById( "nav_d1_1_" + ( x + 1 ) ).innerHTML = lan[ x ];
+    }
+}
+
 function nav_c1_1() {
-    console.log( "click!" );
-    var x = document.getElementById( "nav_c1_1_1" );
-    var x1 = document.getElementById( "nav_c1_1" );
-    var y = document.getElementById( "nav_c1_2_1" );
-    var y1 = document.getElementById( "nav_c1_2" );
-    var z = document.getElementById( "nav_c1_3_1" );
-    var z1 = document.getElementById( "nav_c1_3" );
-
-    if( x1.style.display === "flex" ) {
-        x1.style.backgroundColor = "#232933";
-        y1.style.display = "flex";
-        z1.style.display = "flex";
-    } else {
-        x.style.display = "block";
-    }
+    console.log( "click Gral boton!" );
+    x1.style.display = "none";
+    w1.style.display = "flex";
+    y1.style.display = "flex";
+    z1.style.display = "flex";
 }
 
-function navb2b4() {
-    var x = document.getElementById( "nav-d" );
-
-    if( x.style.display === "block" ) {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
-
-function sectiona2() {
-    var y = document.getElementById( "sectiona3" );
-    var w = document.getElementById( "s-a2" );
-    var z = document.getElementById( "s-a3" );
+function nav_c1_2() {
+    x1.style.display = "flex";
+    x2.style.backgroundImage = "url( 'tools/united-kingdom.png' )";
+    w1.style.display = "none";
+    y1.style.display = "none";
+    z1.style.display = "none";
     
-    if( y.style.display === "block" ) {
-        y.style.display = "none";
-        w.style.display = "inline";
-        z.style.display = "none";
-    } else {
-        y.style.display = "block";
-        w.style.display = "none";
-        z.style.display = "inline";
-    }
+    console.log( "Change to english language" );
+    changeL( engL );
 }
 
-function sectiona3() {
-    var y = document.getElementById( "sectiona3" );
-    var w = document.getElementById( "s-a2" );
-    var z = document.getElementById( "s-a3" );
+function nav_c1_3() {
+    x1.style.display = "flex";
+    x2.style.backgroundImage = "url( 'tools/ukraine.png' )";
+    w1.style.display = "none";
+    y1.style.display = "none";
+    z1.style.display = "none";
     
-    if( y.style.display === "block" ) {
-        y.style.display = "none";
-        w.style.display = "inline";
-        z.style.display = "none";
+    console.log( "Change to ukrainian language" );
+    changeL( ukrL );
+}
+
+function nav_c1_4() {
+    x1.style.display = "flex";
+    x2.style.backgroundImage = "url( 'tools/russia.png' )";
+    w1.style.display = "none";
+    y1.style.display = "none";
+    z1.style.display = "none";
+    
+    console.log( "Change to russian language" );
+    changeL( rusL );
+}
+
+function nav_d1_2() {
+    if( x1.style.display == "none" ) {
+        x1.style.display = "flex";
+        v1.style.display = "none";
+
+        u1.style.display = "block";
+        u2.style.display = "block";
+        u3.style.display = "block";
+        u4.style.display = "block";
+        u5.style.display = "block";
+        u6.style.display = "block";
+        u7.style.display = "block";
+
+        t1.style.height = "40px";
+
+        v2.style.backgroundColor = "#FFFFFF";
     } else {
-        y.style.display = "block";
-        w.style.display = "none";
-        z.style.display = "inline";
+        x1.style.display = "none";
+        v1.style.display = "flex";
+
+        u1.style.display = "none";
+        u2.style.display = "none";
+        u3.style.display = "none";
+        u4.style.display = "none";
+        u5.style.display = "none";
+        u6.style.display = "none";
+        u7.style.display = "none";
+
+        t1.style.height = "30px";
+        t1.style.backgroundColor = "#FFFFFF";
+        t1.style.borderRadius = "15px";
+
+        v2.style.backgroundColor = "#232933";
     }
 }
